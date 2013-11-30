83b065a6aaf018989157d6352966e4dbd73f8f61
ManT1227 <542579890@qq.com>
2013-11-30 23:43:05 +0800

    管理员页面--添加商品分类
    。。

--- 软件产品工程/目标代码/管理员/管理员页面_version_0.1/商品管理_files/axurerp_pagespecificscript.js
+++ 软件产品工程/目标代码/管理员/管理员页面_version_0.1/商品管理_files/axurerp_pagespecificscript.js
@@ -1,30 +1,73 @@
-﻿for(var i = 0; i < 49; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+﻿for(var i = 0; i < 72; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
 
 });
-
-u31.style.cursor = 'pointer';
-$axure.eventManager.click('u31', function(e) {
+gv_vAlignTable['u21'] = 'center';
+u51.style.cursor = 'pointer';
+$axure.eventManager.click('u51', function(e) {
 
 if (true) {
 
-                                ExpandNode('u31', 'cncu31', 'u32');
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('删除商品.html'));
+    if (!reload) {
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('删除商品.html');
+    } else {
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('删除商品.html'));
+    }
+
 }
 });
 
-u36.style.cursor = 'pointer';
-$axure.eventManager.click('u36', function(e) {
+u25.style.cursor = 'pointer';
+$axure.eventManager.click('u25', function(e) {
 
 if (true) {
 
-    objIframe = document.getElementById("u45");
-    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('商品库存.html'));
+                                ExpandNode('u25', 'cncu25', 'u26');
+}
+});
+u6.tabIndex = 0;
+
+u6.style.cursor = 'pointer';
+$axure.eventManager.click('u6', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('主页.html');
+
+}
+});
+gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u32'] = 'center';
+u62.style.cursor = 'pointer';
+$axure.eventManager.click('u62', function(e) {
+
+if (true) {
+
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('修改商品信息.html'));
     if (!reload) {
-	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('商品库存.html');
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('修改商品信息.html');
     } else {
-        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('商品库存.html'));
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('修改商品信息.html'));
+    }
+
+}
+});
+gv_vAlignTable['u53'] = 'center';gv_vAlignTable['u1'] = 'center';
+u30.style.cursor = 'pointer';
+$axure.eventManager.click('u30', function(e) {
+
+if (true) {
+
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('日用品.html'));
+    if (!reload) {
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('日用品.html');
+    } else {
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('日用品.html'));
     }
 
 }
@@ -38,34 +81,18 @@ if (true) {
                                 ExpandNode('u17', 'cncu17', 'u18');
 }
 });
+gv_vAlignTable['u64'] = 'center';gv_vAlignTable['u19'] = 'center';gv_vAlignTable['u41'] = 'center';u71.tabIndex = 0;
 
-u28.style.cursor = 'pointer';
-$axure.eventManager.click('u28', function(e) {
-
-if (true) {
-
-    objIframe = document.getElementById("u45");
-    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('删除商品.html'));
-    if (!reload) {
-	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('删除商品.html');
-    } else {
-        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('删除商品.html'));
-    }
-
-}
-});
-gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u21'] = 'center';u6.tabIndex = 0;
-
-u6.style.cursor = 'pointer';
-$axure.eventManager.click('u6', function(e) {
+u71.style.cursor = 'pointer';
+$axure.eventManager.click('u71', function(e) {
 
 if (true) {
 
-	self.location.href=$axure.globalVariableProvider.getLinkUrl('主页.html');
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('评价管理.html');
 
 }
 });
-gv_vAlignTable['u6'] = 'top';u15.tabIndex = 0;
+gv_vAlignTable['u71'] = 'top';u15.tabIndex = 0;
 
 u15.style.cursor = 'pointer';
 $axure.eventManager.click('u15', function(e) {
@@ -76,7 +103,155 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u13'] = 'center';u14.tabIndex = 0;
+gv_vAlignTable['u15'] = 'top';
+u45.style.cursor = 'pointer';
+$axure.eventManager.click('u45', function(e) {
+
+if (true) {
+
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('生鲜.html'));
+    if (!reload) {
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('生鲜.html');
+    } else {
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('生鲜.html'));
+    }
+
+}
+});
+
+u36.style.cursor = 'pointer';
+$axure.eventManager.click('u36', function(e) {
+
+if (true) {
+
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('电器.html'));
+    if (!reload) {
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('电器.html');
+    } else {
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('电器.html'));
+    }
+
+}
+});
+gv_vAlignTable['u58'] = 'center';
+u22.style.cursor = 'pointer';
+$axure.eventManager.click('u22', function(e) {
+
+if (true) {
+
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('商品列表.html'));
+    if (!reload) {
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('商品列表.html');
+    } else {
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('商品列表.html'));
+    }
+
+}
+});
+gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u50'] = 'center';gv_vAlignTable['u24'] = 'center';
+u54.style.cursor = 'pointer';
+$axure.eventManager.click('u54', function(e) {
+
+if (true) {
+
+                                ExpandNode('u54', 'cncu54', 'u55');
+}
+});
+
+u39.style.cursor = 'pointer';
+$axure.eventManager.click('u39', function(e) {
+
+if (true) {
+
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('数码.html'));
+    if (!reload) {
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('数码.html');
+    } else {
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('数码.html'));
+    }
+
+}
+});
+u69.tabIndex = 0;
+
+u69.style.cursor = 'pointer';
+$axure.eventManager.click('u69', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('订单管理.html');
+
+}
+});
+gv_vAlignTable['u69'] = 'top';gv_vAlignTable['u61'] = 'center';gv_vAlignTable['u35'] = 'center';
+u65.style.cursor = 'pointer';
+$axure.eventManager.click('u65', function(e) {
+
+if (true) {
+
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('商品销量.html'));
+    if (!reload) {
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('商品销量.html');
+    } else {
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('商品销量.html'));
+    }
+
+}
+});
+gv_vAlignTable['u56'] = 'center';gv_vAlignTable['u5'] = 'center';
+u42.style.cursor = 'pointer';
+$axure.eventManager.click('u42', function(e) {
+
+if (true) {
+
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('文具.html'));
+    if (!reload) {
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('文具.html');
+    } else {
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('文具.html'));
+    }
+
+}
+});
+
+u33.style.cursor = 'pointer';
+$axure.eventManager.click('u33', function(e) {
+
+if (true) {
+
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('家纺.html'));
+    if (!reload) {
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('家纺.html');
+    } else {
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('家纺.html'));
+    }
+
+}
+});
+
+u48.style.cursor = 'pointer';
+$axure.eventManager.click('u48', function(e) {
+
+if (true) {
+
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('新增商品.html'));
+    if (!reload) {
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('新增商品.html');
+    } else {
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('新增商品.html'));
+    }
+
+}
+});
+gv_vAlignTable['u67'] = 'center';u14.tabIndex = 0;
 
 u14.style.cursor = 'pointer';
 $axure.eventManager.click('u14', function(e) {
@@ -87,90 +262,19 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u44'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u27'] = 'center';
-u39.style.cursor = 'pointer';
-$axure.eventManager.click('u39', function(e) {
+gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u44'] = 'center';gv_vAlignTable['u29'] = 'center';
+u59.style.cursor = 'pointer';
+$axure.eventManager.click('u59', function(e) {
 
 if (true) {
 
-    objIframe = document.getElementById("u45");
-    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('修改商品信息.html'));
+    objIframe = document.getElementById("u68");
+    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('商品库存.html'));
     if (!reload) {
-	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('修改商品信息.html');
+	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('商品库存.html');
     } else {
-        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('修改商品信息.html'));
+        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('商品库存.html'));
     }
 
 }
 });
-gv_vAlignTable['u35'] = 'center';
-u42.style.cursor = 'pointer';
-$axure.eventManager.click('u42', function(e) {
-
-if (true) {
-
-    objIframe = document.getElementById("u45");
-    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('商品销量.html'));
-    if (!reload) {
-	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('商品销量.html');
-    } else {
-        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('商品销量.html'));
-    }
-
-}
-});
-gv_vAlignTable['u24'] = 'center';
-u25.style.cursor = 'pointer';
-$axure.eventManager.click('u25', function(e) {
-
-if (true) {
-
-    objIframe = document.getElementById("u45");
-    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('新增商品.html'));
-    if (!reload) {
-	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('新增商品.html');
-    } else {
-        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('新增商品.html'));
-    }
-
-}
-});
-u46.tabIndex = 0;
-
-u46.style.cursor = 'pointer';
-$axure.eventManager.click('u46', function(e) {
-
-if (true) {
-
-	self.location.href=$axure.globalVariableProvider.getLinkUrl('订单管理.html');
-
-}
-});
-gv_vAlignTable['u46'] = 'top';gv_vAlignTable['u19'] = 'center';gv_vAlignTable['u5'] = 'center';u48.tabIndex = 0;
-
-u48.style.cursor = 'pointer';
-$axure.eventManager.click('u48', function(e) {
-
-if (true) {
-
-	self.location.href=$axure.globalVariableProvider.getLinkUrl('评价管理.html');
-
-}
-});
-gv_vAlignTable['u48'] = 'top';
-u22.style.cursor = 'pointer';
-$axure.eventManager.click('u22', function(e) {
-
-if (true) {
-
-    objIframe = document.getElementById("u45");
-    var reload = FrameWindowNeedsReload(objIframe, $axure.globalVariableProvider.getLinkUrl('商品列表.html'));
-    if (!reload) {
-	    objIframe.src=$axure.globalVariableProvider.getLinkUrl('商品列表.html');
-    } else {
-        objIframe.src="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl('商品列表.html'));
-    }
-
-}
-});
-gv_vAlignTable['u33'] = 'center';
